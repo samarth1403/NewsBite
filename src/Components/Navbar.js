b2d398ab04ddcca197a1e786b34afefeb1f8386d
Samarth Ikkalaki <samarthikkalaki123@gmail.com>
2023-01-15 21:38:59 +0530

    Front-end Ready

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -53,10 +53,10 @@ const Navbar = () =>{
           <a className="nav-link disabled">Disabled</a>
         </li> */}
               </ul>
-              <form className="d-flex" role="search">
+              {/* <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Kaam karayach ahe" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      </form> */}
             </div>
           </div>
         </nav>
